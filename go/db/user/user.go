4d0e1d6ed8f9daf20ee97cac1e97201d07dcb940
Nathan Chambers <nathan.m.chambers@gmail.com>
2017-12-31 00:51:18 +1100

    Initial SQL query for users

--- go/db/user/user.go
+++ go/db/user/user.go
@@ -1,15 +1,35 @@
 package user
 
 import (
-	"fmt"
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
 )
 
+//User object for db query
+type User struct {
+	ID               int    `db:"id"`
+	Steamid          string `db:"steamid"`
+	Tsdbid           string `db:"tsdbid"`
+	Email            string `db:"email"`
+	Joindate         string `db:"joindate"`
+	Dob              string `db:"dob"`
+	Gender           string `db:"gender"`
+	Admin            int    `db:"admin"`
+	AttendenceCredit int    `db:"attendenceCredit"`
+	Password         string `db:"password"`
+}
+
 //GetAll returns a list of all users from the database
-func GetAll() {
-	fmt.Printf("Get all users\n")
+func GetAll(db *sql.DB) (result []User, err error) {
+	dbx := sqlx.NewDb(db, "mysql")
+	err = dbx.Select(&result, "SELECT * FROM user")
+	return
 }
 
 //GetSingle returns a single user from the database
-func GetSingle(id int) {
-	fmt.Printf("Get user with id:%d \n", id)
+func GetSingle(id int, db *sql.DB) (result User, err error) {
+	dbx := sqlx.NewDb(db, "mysql")
+	err = dbx.Select(&result, "SELECT * FROM user WHERE id=?", id)
+	return
 }
--- go/main.go
+++ go/main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/SOCOMD/staff/go/db/user"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	db *sql.DB
 )
 
 func main() {
+	var err error
+
+	dbHost := os.Getenv("DBHOST")
+	dbUser := os.Getenv("DBUSER")
+	dbPass := os.Getenv("DBPASS")
+	dbName := os.Getenv("DBNAME")
+
+	db, err = sql.Open("mysql", dbUser+":"+dbPass+"@tcp("+dbHost+":3306)/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	users, err := user.GetAll(db)
+	if err != nil {
+		fmt.Printf("%s\n", err.Error())
+	}
+
+	for _, v := range users {
+		fmt.Println("User Found", " ID:", v.ID, " SteamID:", v.Steamid, " TSDBID:", v.Tsdbid, " PASS:", v.Password)
+	}
+
 	http.HandleFunc("/", handler)
 	log.Println("Listening on localhost:8080")
-	user.GetAll()
-	user.GetSingle(1)
 	http.ListenAndServe(":8080", nil)
 }
 
